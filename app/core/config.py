7fd7ab75f7db827eeccbc2725636df0b9649676b
MaksimKislyakov <maksimkislakov35@gmail.com>
2026-05-25 22:48:56 +0500

    chore: добавить проверку на .env файл на сервере и его создание при отсутсвии

--- app/core/config.py
+++ app/core/config.py
@@ -29,14 +29,12 @@ class Settings(BaseSettings):
     MINIO_SECURE: bool
     MINIO_EXTERNAL_ENDPOINT: str
 
-    # Для pydantic-settings v2+ рекомендуется использовать model_config
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore"
     )
 
-    # ВАЖНО: Добавляем свойство DATABASE_URL, так как ваш env.py его требует!
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
